6399d28784fd98b6da19ecdc2e44068eb1a2acdc
sanchezl391 <sanchezl391@gmail.com>
2016-12-20 13:50:27 -0700

    Playing with functions and scopes

---
+++ Closures And Scopes/Ch3Practice.js
@@ -0,0 +1,12 @@
+var obj = {
+  printOdd:function(max){
+    for(var i =0;i <= max;i++){
+      console.log(i);
+    }
+  }
+};
+obj.printOdd(20);
+(function createAnArray(){
+  var arr = [ 1 , 2 , 3];
+  console.log(arr);
+});
