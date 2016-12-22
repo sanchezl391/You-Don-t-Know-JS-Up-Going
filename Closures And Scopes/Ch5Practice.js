19ad030cf4e1765eef14f40f375e4cfa2e44f1dc
sanchezl391 <sanchezl391@gmail.com>
2016-12-21 22:19:13 -0700

    Important closure concepts

--- Closures And Scopes/Ch5Practice.js
+++ Closures And Scopes/Ch5Practice.js
@@ -1,9 +1,11 @@
-function out(){
-  var a = 10;
-  function i(){
-    console.log(a);
-  }
-  return i;
+/*Will display 5 a bunch of times. This is for various reasons
+  1. JS only has function Scopes
+  2. setTimeout() is scoping to the outside scope of the loop
+  3. since we are outside the for loop there is only one i value
+  4. So, the code can be interpreted as 5 setTimeout stacked with the value i
+*/
+for (var i=1; i<=5; i++) {
+    setTimeout( function timer(){
+        console.log( i );
+    }, i*1000 );
 }
-var obj = out();
-obj();
