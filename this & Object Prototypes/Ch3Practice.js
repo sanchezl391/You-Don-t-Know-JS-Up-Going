eeea2107cdc3f7cb2733240cfd077e9360a272ce
sanchezl391 <sanchezl391@gmail.com>
2017-01-07 20:31:22 -0700

    Added more code on Objects

--- this & Object Prototypes/Ch3Practice.js
+++ this & Object Prototypes/Ch3Practice.js
@@ -5,7 +5,10 @@ console.log(obj);
 obj.a = 'chicken';
 console.log(obj2);  //chicken
 
+var newArr = [10];
+obj.arr = newArr[0];
 var newObj = Object.assign({} , obj);
 console.log(newObj);
 obj.a = "This should not be copied";
+newArr[1] = 20;
 console.log(newObj);
